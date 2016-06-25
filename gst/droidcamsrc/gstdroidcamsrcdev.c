03d59e88df8dd98ebe85ea2fb7a949e4526910ee
Matti Lehtimäki <matti.lehtimaki@gmail.com>
2016-06-25 15:36:47 +0300

    droidcamsrc: Store metadata in buffers only if it's supported.

--- gst/droidcamsrc/gstdroidcamsrcdev.c
+++ gst/droidcamsrc/gstdroidcamsrcdev.c
@@ -873,13 +873,6 @@ gst_droidcamsrc_dev_start_video_recording (GstDroidCamSrcDev * dev)
   dev->vid->video_frames = 0;
   dev->vid->queued_frames = 0;
 
-  /* TODO: get that from caps */
-  if (!droid_media_camera_store_meta_data_in_buffers (dev->cam, true)) {
-    GST_ELEMENT_ERROR (src, LIBRARY, SETTINGS,
-        ("error storing meta data in buffers for video recording"), (NULL));
-    goto out;
-  }
-
   if (!droid_media_camera_start_recording (dev->cam)) {
     GST_ELEMENT_ERROR (src, LIBRARY, FAILED, ("error starting video recording"),
         (NULL));
